0585f144af09e0b363fca6765093d0c4d4eb4572
Matthew Kim <matthewkim@Aquaphobic-Fish.local>
2023-04-23 17:25:48 -0400

    stuff

---
+++ components/TaskList.jsx
@@ -0,0 +1,11 @@
+function TaskList({tasks}) {
+     return (
+          <ul>
+               {tasks.map((task, index) => (
+                    <li key={indexedDB}>{task}</li>
+               ))}
+          </ul>
+     )
+}
+
+export default TaskList;
